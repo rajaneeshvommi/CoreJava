ae4fa4582bd480ce74e27a32dc7b9178c2dc6916
rajaneesh <rajaneesh.vommi@gmail.com>
2017-04-22 19:59:53 +0530

    SmallChanges

--- Check_prime.java
+++ Check_prime.java
@@ -1,25 +1,25 @@
 package logics;
-
 import java.util.Scanner;
-
+ // To check number it is Prime or not
 public class Check_prime {
 
 	public static void main(String[] args) {
 		int prime =0;
+		
 		System.out.println("Enter the value:");
 		Scanner S = new Scanner(System.in);
-		int len = S.nextInt();
+		int len = S.nextInt(); // input value
+		
 		for(int i=2;i<len;i++){
 			if(len%i==0){
 				prime =1;
 				break;
 					}
-				}
+				}; // loop condition
 			if(prime==0){
 				System.out.println("It is a prime Number");
 			}else{
 				System.out.println("It is a Not prime Number");
-				
-			}
-	};
-};
+				}
+			}; //End of Main
+};//End of Class
--- Fabinacci_Series.java
+++ Fabinacci_Series.java
@@ -1,10 +1,20 @@
 package logics;
 
+import java.util.Scanner;
+
 public class Fabinacci_Series {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
-}
+		System.out.println("Enter the value:");
+		Scanner S = new Scanner(System.in);
+		int len= S.nextInt();
+		int  n1=1,n2=2,n3;
+		System.out.print(n1 +" "+ n2+" ");
+		for(int i=1;i<=len;i++){
+			n3 =n1+ n2;
+			System.out.print(n3+" ");
+			n1=n2;
+			n2=n3;
+		};// End of Loop
+	};//End of Main
+};// End of Class
